118ee7e195b9aa4cce7c63c401661637393e2e2b
nkleinmann <nekleinmann@gmail.com>
2020-12-31 16:05:46 -0500

    carousel and card working together

--- src/components/projects/projectInfo.js
+++ src/components/projects/projectInfo.js
@@ -1,5 +1,6 @@
 import TeamBuilder from "../../images/TeamBuilder.jpg";
 import WhileYoureThere from "../../images/WhileYoureThere.jpg";
+import burger from "../../images/burger.jpg"
 
 const projectInfo = [
     {
@@ -8,6 +9,7 @@ const projectInfo = [
         githubRepo: "https://github.com/nkleinmann/While-Youre-There",
         image: WhileYoureThere,
         description: "A shopping list app designed for groups to be efficient shoppers (now more important than ever with Covid-19).",
+        techUsed: "React, Font Awesome and Animate.css, MySQL/Sequelize, Auth0, Heroku",
     },
     {
         title: "Team Builder",
@@ -15,6 +17,15 @@ const projectInfo = [
         githubRepo: "https://github.com/nkleinmann/the-team-builders",
         image: TeamBuilder,
         description: "An app that brings remote teams together.",
+        techUsed: "HTML/Bootstrap CSS, Font Awesome/Mimic.css, Express Handlebars, jQuery/Auth0, ExpressJS/Sessions, File Stack, Heroku, MySQL/Sequelize, Passport",
+    },
+    {
+        title: "Eat Da Burger!",
+        deployedSite: "https://pure-earth-06219.herokuapp.com/",
+        githubRepo: "https://github.com/nkleinmann/NK-Eat-Da-Burger",
+        image: burger,
+        description: "Restaurant application that allows users to input burger names and note if they have devoured the burgers or not. Users can input burgers, delete, and devour burgers.",
+        techUsed: "HTML, CSS, JavaScript, jQuery, Node, Express, Handlebars, MySQL, and a homemade ORM (yum!)",
     },
 ];
 
--- src/views/projects.jsx
+++ src/views/projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Col from "react-bootstrap/Col";
 import projectInfo from "../components/projects/projectInfo";
 import Image from "react-bootstrap/image";
 import Carousel from 'react-bootstrap/Carousel';
@@ -10,52 +10,48 @@ import Card from 'react-bootstrap/Card';
 
 function Projects() {
     // console.log(projectInfo)
+    // const userSelect = useRef(null);
+
     return (
         <div>
-            {/* <a href="/">
-                <div className="mt-5 text-center">
-                    <i className="fas fa-chevron-up" />
-                </div>
-            </a> */}
             <Row className="justify-content-center">
-            <Carousel className="carousel">
-            {projectInfo.map((project) => {
-                return (
-                    // <div key={project.title} className="mb-5 projectBackground text-white">
-                    // <Row>
-                    //     <Col className="text-center">
-                    //         <h2><strong>{project.title}</strong></h2>
-                    //     </Col>
-                    // </Row>
-                    // <Row>
-                    //     <Col className="text-center">
-                    //         <Image className="projectImage" src={project.image} alt={project.title} />
-                    //     </Col>
-                    //     <Col className="text-center">
-                    //        <p className="projectText">{project.description}</p>
-                    //     </Col>
-                    // </Row>
-                    // </div>
-                       <Carousel.Item key={project.title} className="text-center">
-                           <Card style={{ width: '18rem' }}>
-                               <Card.Img variant="top" src={project.image} />
-                               <Card.Body>
-                                   <Card.Title>{project.title}</Card.Title>
-                                   <Card.Text>
-                                       {project.description}
-                                   </Card.Text>
-                                   <Card.Link href={project.deployedSite} target="_blank">
-                                       Deployed Site
-                                   </Card.Link>
-                                   <Card.Link href={project.githubRepo} target="_blank">
-                                       Github Repository
-                                   </Card.Link>
-                               </Card.Body>
-                           </Card>
-                       </Carousel.Item>
-                )
-            })}
-            </Carousel>
+                <Carousel className="carousel rounded">
+                    {projectInfo.map((project) => {
+                        return (
+                            // For list of projects
+                            // <div key={project.title} className="mb-5 projectBackground text-white">
+                            // <Row>
+                            //     <Col className="text-center">
+                            //         <h2><strong>{project.title}</strong></h2>
+                            //     </Col>
+                            // </Row>
+                            // <Row>
+                            //     <Col className="text-center">
+                            // <Image className="projectImage" src={project.image} alt={project.title} />
+                            //     </Col>
+                            //     <Col className="text-center">
+                            //        <p className="projectText">{project.description}</p>
+                            //     </Col>
+                            // </Row>
+                            // </div>
+                            // ref={createRef()}
+                            <Carousel.Item key={project.title} className="text-center">
+                                <Image className="projectImage" src={project.image} alt={project.title} />
+                                <Card className="mt-3">
+                                    <Card.Body>
+                                    <h3>{project.title}</h3>
+                                    <p>{project.description}</p>
+                                    <p>
+                                        Technology Used:
+                                       <br />
+                                        {project.techUsed}
+                                    </p>
+                                    </Card.Body>
+                                </Card>
+                            </Carousel.Item>
+                        )
+                    })}
+                </Carousel>
             </Row>
             <a href="/about">
                 <div className="mt-5 text-center">
