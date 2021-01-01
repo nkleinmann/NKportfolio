7f99e597724559cc1050a341c08a2e09444f9967
nkleinmann <nekleinmann@gmail.com>
2021-01-01 14:05:50 -0500

    skiills table

---
+++ src/components/skillsTable.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function SkillsTable() {
+    return (
+        <div className="skills rounded">
+            <Row>
+                <Col sm={12} className="m-3 text-center">
+                    <h3>Skills &amp; Technologies:</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col >
+                    Languages <i className="fa fa-code fa-1x"></i>
+                    <br />
+                    <p className="mt-3">HTML, CSS, JavaScript, JSX</p>
+                    
+                </Col>
+                <Col>
+                    Tools <i className="fa fa-terminal fa-1x"></i>
+                    <br />
+                    <p className="mt-3">MongoDB, MySQL, Git, Github</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    Frameworks <i class="fas fa-code-branch"></i>
+                    <br />
+                    <p className="mt-3">React, Express, Bootstrap, Materialize</p>
+                </Col>
+                <Col>
+                    Other Skills <i class="far fa-handshake"></i>
+                    <br />
+                    <p className="mt-3">Teamwork, Problem Solving, Communication, Creative Thinking</p>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default SkillsTable;
--- src/views/about.jsx
+++ src/views/about.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import SkillsTable from "../components/skillsTable";
 
 function About() {
     return (
         <div className="mt-5 animate__animated animate__slideInUp">
-            <Row>
-                <Col sm={12} md={6} className="m-3">
-                    <p>Skills:</p>
-                    <ul>
-                        <li>React</li>
-                    </ul>
+            <Row className="d-flex justify-content-center">
+                <Col sm={12} md={6} className="m-3 text-center">
+                    <SkillsTable />
                 </Col>
-                <Col sm={12} md={4} className="m-3 text-center aboutMe align-middle">
+                <Col sm={12} md={4} className="mt-5 text-center aboutMe align-middle">
                     <p>
                         Passionate about web development, math and computer science education, and being active.
                     </p>
@@ -20,9 +18,6 @@ function About() {
                     </p>
                 </Col>
             </Row>
-
-
-
             <a href="/contact">
                 <div className="mt-5 text-center">
                     <i className="fas fa-chevron-down" />
