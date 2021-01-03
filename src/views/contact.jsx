41ef00f044d4b7a19367d1fd9f735c8ee0e5dc58
nkleinmann <nekleinmann@gmail.com>
2021-01-03 18:13:22 -0500

    cleaned code

--- src/views/contact.jsx
+++ src/views/contact.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import NicoleKleinmannResume from "../assets/docs/NicoleKleinmannResume.docx.pdf";
 
 
 function Contact() {
